b6dc3c80bc18484b60772baffdb5cf80eedbb70c
brunty <matt@mfyu.co.uk>
2017-06-16 21:18:17 +0100

    Refactor outputter

--- src/Outputter.php
+++ src/Outputter.php
@@ -4,23 +4,22 @@ namespace Brunty\Cigar;
 
 class Outputter
 {
-    public function output(array $results, bool $quiet = false)
+    const CONSOLE_GREEN = "\033[32m";
+    const CONSOLE_RED = "\033[31m";
+    const CONSOLE_RESET = "\033[0m";
+
+    const SYMBOL_PASSED = '✓';
+    const SYMBOL_FAILED = '✘';
+
+    public function output(array $results, bool $quiet = false): bool
     {
         $suitePassed = true;
         ob_start();
         foreach ($results as $result) {
-            $passed = $result->passed();
-            $colour = "\033[32m"; // green
-            $status = "✓";
+            [$colour, $status, $suitePassed] = $this->getOutputAndReturn($result);
 
-            if ( ! $passed) {
-                $suitePassed = false;
-                $colour = "\033[31m"; // red
-                $status = "✘";
-            }
-
-            if( ! $quiet) {
-                echo "{$colour}{$status} {$result->getDomain()->getUrl()} [{$result->getDomain()->getStatus()}:{$result->getStatusCode()}] \033[0m" . PHP_EOL;
+            if ( ! $quiet) {
+                $this->outputLine($colour, $status, $result);
             }
             ob_flush();
         }
@@ -28,5 +27,25 @@ class Outputter
         ob_end_flush();
 
         return $suitePassed;
+    }
+
+    private function getOutputAndReturn(Result $result): array
+    {
+        $passed = $result->passed();
+        $suitePassed = $passed;
+        $colour = self::CONSOLE_GREEN;
+        $status = self::SYMBOL_PASSED;
+
+        if ( ! $passed) {
+            $colour = self::CONSOLE_RED;
+            $status = self::SYMBOL_FAILED;
+        }
+
+        return [$colour, $status, $suitePassed];
+    }
+
+    private function outputLine(string $colour, string $status, Result $result): void
+    {
+        echo "{$colour}{$status} {$result->getDomain()->getUrl()} [{$result->getDomain()->getStatus()}:{$result->getStatusCode()}] " . self::CONSOLE_RESET . PHP_EOL;
     }
 }
