25acc83fc0a267692cdf87f559794ba8970ff2e5
Matt Brunt <matt@mfyu.co.uk>
2018-03-12 13:31:39 +0000

    Add base URL as argument to the tool

--- src/Parser.php
+++ src/Parser.php
@@ -4,7 +4,22 @@ namespace Brunty\Cigar;
 
 class Parser
 {
+    /**
+     * @var string
+     */
+    private $baseUrl;
 
+    public function __construct(string $baseUrl = null)
+    {
+        $this->baseUrl = rtrim($baseUrl, '/');
+    }
+
+    /**
+     * @param string $filename
+     *
+     * @return Url[]
+     * @throws \ParseError
+     */
     public function parse(string $filename): array
     {
         $urls = json_decode(file_get_contents($filename), true);
@@ -14,7 +29,30 @@ class Parser
         }
 
         return array_map(function($value) {
-            return new Url($value['url'], $value['status'], $value['content'] ?? null);
+            $url = $this->getUrl($value['url']);
+
+            return new Url($url, $value['status'], $value['content'] ?? null);
         }, $urls);
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     * @throws \ParseError
+     */
+    private function getUrl(string $url): string
+    {
+        $urlParts = parse_url($url);
+
+        if ($urlParts === false) {
+            throw new \ParseError("Could not parse URL: $url");
+        }
+
+        if ($this->baseUrl !== null && ! isset($urlParts['host'])) {
+            $url = $this->baseUrl . '/' . ltrim($url, '/');
+        }
+
+        return $url;
     }
 }
